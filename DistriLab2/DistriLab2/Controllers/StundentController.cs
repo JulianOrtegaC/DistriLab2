12f99f9c6a2b4e368261dbef87a559ad79756505
JulianOrtegaC <julianortegacastillo@gmail.com>
2023-03-22 17:56:46 -0500

    Patch Student

--- DistriLab2/DistriLab2/Controllers/StundentController.cs
+++ DistriLab2/DistriLab2/Controllers/StundentController.cs
@@ -3,7 +3,7 @@ using DistriLab2.Models;
 using DistriLab2.Models.DB;
 using System;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace DistriLab2.Controllers
 {
@@ -11,6 +11,8 @@ namespace DistriLab2.Controllers
     [Route("[controller]")]
     public class StudentController : ControllerBase
     {
+        private static string TEXT_ALERT_STUDENT_NOT_FOUND = "ESTUDIANTE NO ENCONTRADO";
+        private static string TEXT_ALERT_NDOCUMENT_REGISTERED = "DOCUMENTO DE ESTUDIANTE YA ESTA REGISTRADO";
         private readonly dblab2Context _context;
 
         public StudentController(dblab2Context context)
@@ -44,8 +46,8 @@ namespace DistriLab2.Controllers
         [HttpPost]
         public async Task<ActionResult<Student>> CreateStudent(Student student)
         {
-            if (ExisteDocument(_context, student.NumDocument)) {
-                return BadRequest("El Numero de Documento  ya se encuentra registrado");
+            if (DocumentExists(_context, student.NumDocument)) {
+                return BadRequest(TEXT_ALERT_NDOCUMENT_REGISTERED);
             }
             else
             {
@@ -69,7 +71,7 @@ namespace DistriLab2.Controllers
             do
             {
                 codigoGenerado = Int32.Parse($"{year}{semestre}{contador:D5}");
-                codigoExiste = ExisteCodigo(context, codigoGenerado);
+                codigoExiste = StudentExists(context, codigoGenerado);
 
                 if (codigoExiste)
                 {
@@ -82,11 +84,11 @@ namespace DistriLab2.Controllers
         }
 
 
-        public static bool ExisteCodigo(dblab2Context dbContext, int codigoGenerado)
+        public static bool StudentExists(dblab2Context dbContext, int codigoGenerado)
         {
             return dbContext.Students.Any(e => e.CodStudent == codigoGenerado);
         }
-        public static bool ExisteDocument(dblab2Context dbContext, string document)
+        public static bool DocumentExists(dblab2Context dbContext, string document)
         {
             return dbContext.Students.Any(e => e.NumDocument == document);
         }
@@ -100,7 +102,7 @@ namespace DistriLab2.Controllers
 
             if (update == null)
                 return BadRequest();
-            update.CodStudent = student.CodStudent;
+          
             update.FirstNameStudent = student.FirstNameStudent;
             update.LastNameStudent = student.LastNameStudent;
             update.TypeDocument = student.TypeDocument;
@@ -115,5 +117,185 @@ namespace DistriLab2.Controllers
             }
             return Ok(update);
         }
+
+        /*[HttpPatch]
+        [Route("updateStudent/{codStudent}")]
+        public async Task<IActionResult> updateInscriptionCodeStudent(int codStudent, JsonPatchDocument<Student> student)
+        {
+            var _studentAux = await _context.Students.FindAsync(codStudent);
+            if (_studentAux == null)
+            {
+                return NotFound();
+            }
+
+            student.ApplyTo(_studentAux, (Microsoft.AspNetCore.JsonPatch.Adapters.IObjectAdapter)ModelState);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(_studentAux);
+        }*/
+
+        /*[HttpPatch]
+        [Route("updateStudent/{codStudent}")]
+        public async Task<IActionResult> updateInscriptionCodeStudent(int codStudent, JsonPatchDocument<Student>student)
+        {
+            try
+            {
+                var _studentAux = await _context.Students.FindAsync(codStudent);
+                if (_studentAux == null)
+                {
+                    return BadRequest(TEXT_ALERT_STUDENT_NOT_FOUND);
+                }else
+                if(student.FirstNameStudent !="")
+                    _studentAux.FirstNameStudent = student.FirstNameStudent;
+                if (student.LastNameStudent != "")
+                    _studentAux.LastNameStudent = student.LastNameStudent;
+                if (student.TypeDocument != "")
+                    _studentAux.TypeDocument = student.TypeDocument;
+                if (student.NumDocument != "")
+                    _studentAux.NumDocument = student.NumDocument;
+                if (student.StatusStudent != "")
+                    _studentAux.StatusStudent = student.StatusStudent;
+                if (student.GenderStudent != "")
+                    _studentAux.GenderStudent = student.GenderStudent;
+
+
+
+
+                await _context.SaveChangesAsync();
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }*/
+        [HttpPatch]
+        [Route("updateNameStudent/{CodStudent}")]
+        public async Task<IActionResult> updateNameStudent(int CodStudent,string FirstNameStudent)
+        {
+            try
+            {
+                var student = await _context.Students.FindAsync(CodStudent);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                student.FirstNameStudent = FirstNameStudent;
+                await _context.SaveChangesAsync();
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch]
+        [Route("updateLastNameStudent/{CodStudent}")]
+        public async Task<IActionResult> updateLastNameStudent(int CodStudent, string LastNameStudent)
+        {
+            try
+            {
+                var student = await _context.Students.FindAsync(CodStudent);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                student.LastNameStudent = LastNameStudent;
+                await _context.SaveChangesAsync();
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch]
+        [Route("updateTypeDocumentStudent/{CodStudent}")]
+        public async Task<IActionResult> updateTypeDocumentStudent(int CodStudent, string TypeDocument)
+        {
+            try
+            {
+                var student = await _context.Students.FindAsync(CodStudent);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                student.TypeDocument = TypeDocument;
+                await _context.SaveChangesAsync();
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch]
+        [Route("updateNumDocumentStudent/{CodStudent}")]
+        public async Task<IActionResult> updateNumDocumentStudent(int CodStudent, string NumDocument)
+        {
+            try
+            {
+                var student = await _context.Students.FindAsync(CodStudent);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                student.NumDocument = NumDocument;
+                await _context.SaveChangesAsync();
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch]
+        [Route("updateStatusStudent/{CodStudent}")]
+        public async Task<IActionResult> updateStatusStudent(int CodStudent, string StatusStudent)
+        {
+            try
+            {
+                var student = await _context.Students.FindAsync(CodStudent);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                student.StatusStudent = StatusStudent;
+                await _context.SaveChangesAsync();
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch]
+        [Route("updateGenderStudent/{CodStudent}")]
+        public async Task<IActionResult> updateGenderStudent(int CodStudent, string GenderStudent)
+        {
+            try
+            {
+                var student = await _context.Students.FindAsync(CodStudent);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                student.GenderStudent = GenderStudent;
+                await _context.SaveChangesAsync();
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
     }
 }
