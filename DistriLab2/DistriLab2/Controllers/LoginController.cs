30540aa810025bfcd8de2d8723b7a5cd60c3008c
JulianOrtegaC <Julianortegacastillo@gmail.com>
2023-05-08 14:31:08 -0500

    VALIDATIONS

--- DistriLab2/DistriLab2/Controllers/LoginController.cs
+++ DistriLab2/DistriLab2/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using DistriLab2.Models.DB;
+﻿using Azure.Core;
+using DistriLab2.Models.DB;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Win32;
@@ -29,13 +30,20 @@ namespace DistriLab2.Controllers
             {
                 string auxPassword = ToSHA256(hash);
                 Credential user = _context.Credentials.Where(c => c.EmailUser == emailUser && c.HashUser == auxPassword).FirstOrDefault();
+                User user1 = _context.Users.Where(c => c.EmailUser == emailUser).FirstOrDefault();
                 if (user != null)
                 {
-                    User perAux = _context.Users.Where(p => p.EmailUser == user.EmailUser).FirstOrDefault();
-                    DateTime expirationDate = DateTime.UtcNow.AddMinutes(30);
-                    string token = generateToken(emailUser, expirationDate);
-                    return Ok(new { token = token, user = perAux});
-
+                    if (user1.StatusUser == "A")
+                    {
+                        User perAux = _context.Users.Where(p => p.EmailUser == user.EmailUser).FirstOrDefault();
+                        DateTime expirationDate = DateTime.UtcNow.AddMinutes(30);
+                        string token = generateToken(emailUser, expirationDate);
+                        return Ok(new { token = token, user = perAux });
+                    }
+                    else
+                    {
+                    return BadRequest(new { message = "El usuario está inactivo" });
+                }
                 }
                 else
                 {
@@ -56,27 +64,31 @@ namespace DistriLab2.Controllers
                 return sb.ToString();
             }
 
-            [Route("Register")]
-            [HttpPost]
-            public async Task<IActionResult> Register(Credential credential)
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(Credential credential)
+        {
+            if (!ModelState.IsValid)
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest();
-                }
-                    Credential crede = _context.Credentials.Where(p => p.EmailUser == credential.EmailUser).FirstOrDefault();
-                            string passaux = ToSHA256(credential.HashUser);
-                            Credential cred = new Credential()
-                            {
-                                CodCredential = "2",
-                                EmailUser = credential.EmailUser,
-                                HashUser = passaux
-                            };
-                            _context.Credentials.Add(cred);
-                            await _context.SaveChangesAsync();
-
-                            return Created($"/User/{cred.EmailUser}", cred);
+                return BadRequest();
             }
+            Credential crede = _context.Credentials.Where(p => p.EmailUser == credential.EmailUser).FirstOrDefault();
+            if (crede != null)
+            {
+                string passaux = ToSHA256(credential.HashUser);
+                Credential cred = new Credential()
+                {
+                    CodCredential = (incrementId(_context) + 1) + "A",
+                    EmailUser = credential.EmailUser,
+                    HashUser = passaux
+                };
+                _context.Credentials.Add(cred);
+                await _context.SaveChangesAsync();
+
+                return Created($"/User/{cred.EmailUser}", cred);
+            }
+            return BadRequest(new { message = "La direción de correo eletronico no existe"});
+        }
 
 
             [HttpGet]
@@ -99,5 +111,12 @@ namespace DistriLab2.Controllers
                 string token = tokenHandler.WriteToken(tokenConfig);
                 return token;
             }
-        }
+
+            [HttpGet]
+            public static int incrementId(dblab2Context dbContext)
+            {
+                int count = dbContext.Credentials.Count();
+                return count;
+            }
+    }
 }
